6d0403a490cf4fca60e6241c9f9678d9c1608816
JustShiba <liyalisica9356@gmail.com>
2022-09-03 18:06:30 +0300

    feat: add initial endpoint

---
+++ src/middlewares/index.js
@@ -0,0 +1,9 @@
+const apiKey = require('./apiKey');
+const setupCors = require('./setupCors');
+const errorHandler = require('./errorHandler');
+
+module.exports = {
+  apiKey,
+  setupCors,
+  errorHandler,
+};
